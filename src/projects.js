dcc0d2fc0d246a1a60763bf4c997b38a05c42b74
Christoph Fleschutz <ch.fleschutz@gmail.com>
2023-09-18 19:00:44 +0200

    Updated project descriptions and tags

--- src/projects.js
+++ src/projects.js
@@ -30,30 +30,45 @@ export function setupProjects() {
             a physically-based material system promises further insights into the intricate world of programming a render engine.`,
             imageSrc: painting,
             githubLink: 'https://github.com/chFleschutz/VulkaniteRenderEngine',
-            tags: ['Computer Graphics', 'C++', 'Vulkan API', 'GLSL' , 'GLFW', 'GLM', 'tiny-obj-loader'],
+            tags: ['Vulkan API', 'C++', 'GLSL' , 'GLFW', 'GLM', 'tiny-obj-loader', 'Real-Time Rendering' ],
             imageLeftSide: true
         },
         {
             title: 'Abduction in Lab 42',
-            description: `Abduction in Lab 42 is a Virtual Reality (VR) game developed using the Unity game engine. It was created during our sixth semester in collaboration with a group of fellow students. In this VR experience, players will encounter three distinct levels, each presenting unique challenges and puzzles. <br><br>
-            In the first level, players are tasked with collecting gems hidden within Lab 42's enigmatic chambers. The second level introduces a puzzle element where players must manipulate lasers to hit specific targets. The final level adds an element of defense, as players must protect their robot companion from waves of enemies. <br><br>
-            To deliver an authentic VR experience, we utilized VR headsets and SteamVR controllers to ensure immersive interactions within the game environment. Additionally, all 3D models in the game were created using Blender, adding detail and depth to the world. <br><br>            
-            This project was supervised and evaluated by Prof. Dr. Bichlmeier, our class professor, whose guidance and expertise played a significant role in the development of Abduction in Lab 42. It stands as a testament to our collaborative efforts and dedication to creating an engaging VR gaming experience during our academic journey.`,
+            description: `Abduction in Lab 42 is a Virtual Reality (VR) game developed using the Unity game engine. It was created during our sixth semester 
+            in collaboration with a group of fellow students. In this VR experience, players will encounter three distinct levels, each presenting unique 
+            challenges and puzzles. <br><br>
+            In the first level, players are tasked with collecting hidden gems. The second level introduces a puzzle element where players must manipulate 
+            lasers to hit specific targets. The final level adds an element of defense, requiring players to protect their robot companion from waves of 
+            enemies. <br><br>
+            To deliver an immersive  VR experience, we utilized a Pimax VR headset and Valve Index Controllers, ensuring seamless interactions within the game 
+            environment. Additionally, all 3D models in the game were created using Blender, enhancing the world's detail and depth. <br><br>            
+            This project was supervised and evaluated by Prof. Dr. Bichlmeier, our class professor, whose guidance and expertise significantly contributed to 
+            the game's development. It stands as a testament to our collaborative efforts and dedication to creating an engaging VR gaming experience during 
+            our academic journey.`,
             imageSrc: painting,
             githubLink: 'https://github.com/chFleschutz/abduction-in-lab-42',
-            tags: ['Virtual Reality', 'Unity Game Engine', 'C#', 'Blender', 'Game Development'],
+            tags: ['Unity Game Engine', 'C#', 'Virtual Reality', 'Blender', 'Game Development'],
             imageLeftSide: false
         },
         {
             title: 'Hive Mind',
-            description: `"Hive Mind" is a turn-based strategy game developed with Unreal Engine 5.1.<br><br>
-            The game features procedurally generated hexagonal maps using Perlin noise. This technique ensures the creation of diverse and unique landscapes every time. The map boasts a range of terrain types, including lakes, mountains, and deserts, each with significant gameplay implications; for instance, units cannot traverse mountains or lakes. <br><br>
-            The core gameplay revolves around turn-based mechanics. Players can strategically plan their moves carefully, and units will execute their actions only after the turn has concluded. Additionally, the game incorporates a building system, enabling players to construct structures for resource generation and unit deployment, thus introducing an additional layer of strategic depth and management complexity. Unit movement takes place on a hexagonal grid, providing precise control and demanding thoughtful positioning. This system enriches gameplay and fosters strategic thinking. <br><br>
-            Benefiting from the capabilities of Unreal Engine 5, "Hive Mind" showcases cutting-edge advancements in graphics rendering and game development technology.<br><br>
-            Explore the technical intricacies of "Hive Mind," immersing yourself in the world of procedural generation, hexagonal tile movement, and strategic gameplay, all enhanced by the power and beauty of Unreal Engine 5.`,
+            description: `Hive Mind is a turn-based strategy game developed with Unreal Engine 5.1.<br><br>
+            The game features procedurally generated hexagonal maps using Perlin noise. This technique ensures the creation of diverse and unique landscapes
+            every time. The map boasts a range of terrain types, including lakes, mountains, and deserts, each with gameplay implications. For instance, units 
+            cannot traverse mountains or lakes. <br><br>
+            The core gameplay revolves around turn-based mechanics. Players can strategically plan their moves carefully, and units will execute their 
+            actions only after the turn has concluded. Additionally, the game incorporates a building system, enabling players to construct structures for 
+            resource generation and unit deployment, thus introducing an additional layer of strategic depth and management complexity. Unit movement takes 
+            place on a hexagonal grid, providing precise control and demanding thoughtful positioning. This system enriches gameplay and fosters strategic 
+            thinking. <br><br>
+            Benefiting from the capabilities of Unreal Engine 5, "Hive Mind" showcases cutting-edge advancements in graphics rendering and game development 
+            technology.<br><br>
+            Explore the technical intricacies of "Hive Mind," immersing yourself in the world of procedural generation, hexagonal tile movement, and 
+            strategic gameplay, all enhanced by the power and beauty of Unreal Engine 5.`,
             imageSrc: painting,
-            githubLink: 'https://github.com/chFleschutz/Project_Hive',
-            tags: ['C++', 'Unreal Engine', 'Procedural Generation', 'Blender', 'Game Development'],
+            githubLink: 'https://github.com/chFleschutz/hive-mind',
+            tags: ['Unreal Engine', 'C++', 'Procedural Generation', 'Blender', 'Game Development'],
             imageLeftSide: true
         }
     ]
