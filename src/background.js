93d609eaf0ec9d0c6f7a8f7f5e4de36547b4f26f
Christoph Fleschutz <ch.fleschutz@gmail.com>
2023-09-20 12:09:44 +0200

    Fixed backround increasing scroll length

--- src/background.js
+++ src/background.js
@@ -1,57 +1,33 @@
 import './styles.css'
 
+function createBackgroundLines(topOffset, isLeftSide) {
+    const lines = document.createElement('div');
+    lines.className = 'bg-line-stripe';
+    if (isLeftSide) {
+        lines.style.left = '-50px';
+    } else {
+        lines.style.right = '-50px';
+    }
+
+    lines.style.top = `calc(100vh + ${topOffset}px)`;
+    for (let i = 0; i < 4; i++) {
+        const line = document.createElement('div');
+        line.className = 'bg-line';
+        lines.appendChild(line);
+    }
+    return lines;
+}
+
 export function setupBackground() {
     const background = document.createElement('div');
     background.className = 'background';
-    background.innerHTML = `
-    <div class="bg-line-stripe" style="left: -50px; top: 800px;">
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-    </div>
-    <div class="bg-line-stripe" style="right: -50px; top: 1500px;">
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-    </div>
-    <div class="bg-line-stripe" style="left: -50px; top: 1900px;">
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-    </div>
-    <div class="bg-line-stripe" style="right: -50px; top: 2600px;">
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-    </div>
-    <div class="bg-line-stripe" style="left: -50px; top: 3000px;">
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-    </div>
-    <div class="bg-line-stripe" style="right: -50px; top: 3700px;">
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-    </div>
-    <div class="bg-line-stripe" style="left: -50px; top: 4100px;">
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-    </div>
-    <div class="bg-line-stripe" style="right: -50px; top: 4800px;">
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-        <div class="bg-line"></div>
-    </div>
-    `;
+    background.appendChild(createBackgroundLines(-200, true));
+    background.appendChild(createBackgroundLines(500, false));
+    background.appendChild(createBackgroundLines(800, true));
+    background.appendChild(createBackgroundLines(1500, false));
+    background.appendChild(createBackgroundLines(1900, true));
+    background.appendChild(createBackgroundLines(2600, false));
+    background.appendChild(createBackgroundLines(2900, true));
+    background.appendChild(createBackgroundLines(3600, false));
     return background;
 }
