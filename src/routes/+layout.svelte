dad70dad8fb88ccc25fda94fce75ff4a2036848d
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-12-06 23:27:53 +0100

    Add global css styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,67 @@
 <script>
-    export let title = 'Christoph Fleschutz';
-  </script>
+	import '../app.css';
+</script>
 
 <header>
-    <h1>{title}</h1>
-    <nav>
-      <a href="/">about</a>
-      <a href="/">projects</a>
-      <a href="/">contact</a>
-    </nav>
+	<h1>Christoph Fleschutz</h1>
+	<nav>
+		<a href="/">about</a>
+		<a href="/">projects</a>
+		<a href="/">contact</a>
+	</nav>
 </header>
 
 <main>
-    <slot></slot> 
+	<div class="content">
+        <slot></slot>
+    </div> 
 </main>
 
 <footer>
-    <p>&copy; 2024 Created by Christoph Fleschutz</p>
+	<p>&copy; 2024 Created by Christoph Fleschutz</p>
 </footer>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		max-width: var(--max-width);
+		padding: 1rem;
+		margin: 0 auto;
+        font-size: 20px;
+	}
+
+	header h1 {
+		font-size: 20px;
+		text-transform: uppercase;
+	}
+
+	nav a {
+		text-decoration: none;
+		transition: transform ease 0.2s;
+		margin: 0 1rem;
+	}
+
+	nav a:hover {
+		transform: translate(0, -3px);
+	}
+
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+	}
+
+    .content {
+        width: var(--max-width);
+        text-align: center;
+    }
+
+	footer {
+		text-align: center;
+		padding: 1rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,18 @@
 	<title>{title}</title>
 </svelte:head>
 
-<main>
-	<h1>Welcome to My Portfolio!</h1>
-	<p>This is the main page of my website.</p>
-</main>
+<div class="container">
+    <h1>Welcome to My Portfolio!</h1>
+    <p>This is the main page of my website.</p>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100vh;
+    }
+</style>
