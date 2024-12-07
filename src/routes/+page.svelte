df18376dafc4dbb10138157d474d49862a06d514
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-12-07 10:33:54 +0100

    Add arrow from font-awesome

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+	import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
 	import Header from '$lib/header.svelte';
 </script>
 
@@ -13,6 +16,10 @@
 		<h1>Welcome to My Portfolio!</h1>
 		<p>This is the main page of my website.</p>
 	</div>
+
+	<div class="arrow">
+		<FontAwesomeIcon icon={faChevronDown} />
+	</div>
 </div>
 
 <style>
@@ -22,11 +29,17 @@
 		height: 100vh;
 	}
 
-    .hero {
-        display: flex;
+	.hero {
+		display: flex;
 		flex: 1;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.arrow {
+		font-size: 1rem;
+		text-align: center;
+		padding: 2rem 1rem 3rem 1rem;
+	}
 </style>
