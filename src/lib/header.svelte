7b13b6f78b066a82fc5e2ede8001646828f04bb9
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-12-07 10:17:48 +0100

    Move header to component

---
+++ src/lib/header.svelte
@@ -0,0 +1,39 @@
+<header>
+	<h1>Christoph Fleschutz</h1>
+	<nav>
+		<a href="/">about me</a>
+		<a href="/">projects</a>
+		<a href="/">contact</a>
+	</nav>
+</header>
+
+<style>
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		max-width: var(--max-width);
+		padding: 1rem;
+		gap: 3rem;
+	}
+
+	header h1 {
+		font-size: 20px;
+		text-transform: uppercase;
+	}
+
+	nav {
+		display: flex;
+		white-space: nowrap;
+		gap: 3rem;
+	}
+
+	nav a {
+		text-decoration: none;
+		transition: transform ease 0.2s;
+	}
+
+	nav a:hover {
+		transform: translate(0, -3px);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,13 @@
 <script>
-	export let title = 'Christoph Fleschutz';
+	import Header from '$lib/header.svelte';
 </script>
 
 <svelte:head>
-	<title>{title}</title>
+	<title>Christoph Fleschutz</title>
 </svelte:head>
 
 <div class="container">
-	<header>
-		<h1>Christoph Fleschutz</h1>
-		<nav>
-			<a href="/">about me</a>
-			<a href="/">projects</a>
-			<a href="/">contact</a>
-		</nav>
-	</header>
+	<Header />
 
 	<div class="hero">
 		<h1>Welcome to My Portfolio!</h1>
@@ -23,35 +16,6 @@
 </div>
 
 <style>
-	header {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		max-width: var(--max-width);
-		padding: 1rem;
-        gap: 3rem;
-	}
-
-	header h1 {
-		font-size: 20px;
-		text-transform: uppercase;
-	}
-
-	nav {
-		display: flex;
-		white-space: nowrap;
-        gap: 3rem;
-	}
-
-	nav a {
-		text-decoration: none;
-		transition: transform ease 0.2s;
-	}
-
-	nav a:hover {
-		transform: translate(0, -3px);
-	}
-
 	.container {
 		display: flex;
 		flex-direction: column;
@@ -60,9 +24,9 @@
 
     .hero {
         display: flex;
+		flex: 1;
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        height: 100%;
     }
 </style>
