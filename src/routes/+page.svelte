9b27b074319740ada55aa0e9e2912ce7b4f5ad10
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-12-09 16:58:01 +0100

    Add hero as first section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,14 @@
 	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
 	import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-	import Header from '$lib/header.svelte';
-	import Hero from '$lib/hero.svelte';
+	import Hero from '$lib/sections/hero.svelte';
 </script>
 
 <svelte:head>
 	<title>Christoph Fleschutz</title>
 </svelte:head>
 
-<div class="section">Section 1</div>
+<div class="section"><Hero /></div>
 <div class="section">Section 2</div>
 <div class="section">Section 3</div>
 <div class="section">Section 4</div>
@@ -19,7 +18,7 @@
 	.section {
 		height: 100%;
 		width: 100%;
-		scroll-snap-align: start; 
+		scroll-snap-align: start;
 		display: flex;
 		justify-content: center;
 		align-items: center;
