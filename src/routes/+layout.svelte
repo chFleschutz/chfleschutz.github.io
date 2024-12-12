2bac3333985e0160f360d0e8f1cf13464954fd32
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-12-12 16:19:20 +0100

    Preload google font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,14 +7,17 @@
 </script>
 
 <svelte:head>
-	<!-- Preconnect to Google Fonts -->
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-
-	<!-- Load fonts -->
 	<link
+		rel="preload"
+		as="style"
 		href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap"
+		crossorigin="anonymous"
+	/>
+	<link
 		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap"
 	/>
 </svelte:head>
 
