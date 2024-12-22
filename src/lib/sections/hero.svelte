bd03604f4f5e24572d56917d3608af62f016a27e
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-12-22 17:45:56 +0100

    Add mobile layout

--- src/lib/sections/hero.svelte
+++ src/lib/sections/hero.svelte
@@ -4,36 +4,26 @@
 	import LinkIcons from '$lib/components/link-icons.svelte';
 </script>
 
-<div class="fullpage-container">
-	<div class="hero">
-		<div class="hero-text">
-			<h2>Hello, I'm <span style="color: var(--color-accent)">Chris</span></h2>
-			<p>
-				a dedicated computer science student specializing in game engineering and C++ development
-			</p>
-			<LinkIcons />
-		</div>
+<div class="hero">
+	<div class="hero-text">
+		<h2>Hello, I'm <span style="color: var(--color-accent)">Chris</span></h2>
+		<p>a dedicated computer science student specializing in game engineering and C++ development</p>
+		<LinkIcons />
 	</div>
+
 	<div class="sdf">
 		<SDF />
 	</div>
 </div>
 
-
 <style>
-	.sdf {
-		position: absolute;
-		width: 100%;
-		height: 80vh;
-		z-index: -1;
-	}
-
 	.hero {
-		width: 100%;
+		position: relative;
+		width: 100vw;
+		height: 100vh;
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		gap: 3em;
 	}
 
 	.hero-text {
@@ -53,14 +43,28 @@
 		font-size: var(--font-size-medium);
 	}
 
+	.sdf {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
+	}
+
 	@media (max-width: 768px) {
 		.hero {
 			flex-direction: column-reverse;
-			gap: 0;
+			justify-content: start;
 		}
 
 		.hero-text {
+			width: 80%;
+		}
+
+		.sdf {
+			position: relative;
 			width: 100%;
+			height: auto;
+			aspect-ratio: 1;
 		}
 	}
 </style>
