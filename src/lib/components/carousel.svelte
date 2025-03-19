71f55e276339ac98567c0bd14ae9de2b8c9b8cb9
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2025-03-19 14:38:23 +0100

    Add button indicator

--- src/lib/components/carousel.svelte
+++ src/lib/components/carousel.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import emblaCarouselSvelte from 'embla-carousel-svelte';
+	import { writable } from 'svelte/store';
 	import emblaCarousel from 'embla-carousel';
 	import type { EmblaCarouselType, EmblaOptionsType } from 'embla-carousel';
 
@@ -12,12 +12,20 @@
 	let viewport: HTMLDivElement;
 	let nextButton: HTMLButtonElement;
 	let prevButton: HTMLButtonElement;
+	let dots: HTMLDivElement;
+
+	let snapCount = 0;
+	const selectedDot = writable(0);
 
 	let embla: EmblaCarouselType;
 
 	function onInit() {
 		prevButton.removeAttribute('disabled');
 		nextButton.removeAttribute('disabled');
+
+		snapCount = embla.scrollSnapList().length;
+
+		onSelect();
 	}
 
 	function onSelect() {
@@ -26,19 +34,14 @@
 
 		if (embla.canScrollNext()) nextButton.removeAttribute('disabled');
 		else nextButton.setAttribute('disabled', 'disabled');
-	}
 
-	function onScrollPrev() {
-		embla.scrollPrev();
-	}
-
-	function onScrollNext() {
-		embla.scrollNext();
+		selectedDot.set(embla.selectedScrollSnap());
 	}
 
 	onMount(() => {
 		embla = emblaCarousel(viewport, options);
 		embla.on('init', onInit);
+		embla.on('reInit', onInit);
 		embla.on('select', onSelect);
 	});
 </script>
@@ -52,12 +55,40 @@
 
 	<div class="embla-controls">
 		<div class="embla-buttons">
-			<button class="embla-button" aria-label="Previous" bind:this={prevButton} onclick={onScrollPrev}>
+			<button
+				class="embla-button"
+				aria-label="Previous"
+				bind:this={prevButton}
+				onclick={() => embla.scrollPrev()}
+			>
 				Previous
 			</button>
-			<button class="embla-button" aria-label="Next" bind:this={nextButton} onclick={onScrollNext}>
+			<button
+				class="embla-button"
+				aria-label="Next"
+				bind:this={nextButton}
+				onclick={() => embla.scrollNext()}
+			>
 				Next
 			</button>
+		</div>
+
+		<div class="embla-dots" bind:this={dots}>
+			{#each Array(snapCount) as _, i}
+				{#if $selectedDot === i}
+					<button
+						class="embla-dot embla-dot-selected"
+						aria-label="Go to slide {i + 1}"
+						onclick={() => embla.scrollTo(i)}
+					></button>
+				{:else}
+					<button
+						class="embla-dot"
+						aria-label="Go to slide {i + 1}"
+						onclick={() => embla.scrollTo(i)}
+					></button>
+				{/if}
+			{/each}
 		</div>
 	</div>
 </div>
@@ -81,7 +112,7 @@
 		grid-template-columns: auto 1fr;
 		justify-content: space-between;
 		gap: 1rem;
-		margin-top: 2rem;
+		margin-top: 2.5rem;
 	}
 
 	.embla-buttons {
@@ -97,5 +128,28 @@
 
 	.embla-button:disabled {
 		opacity: 0.5;
+	}
+
+	.embla-dots {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: flex-end;
+		align-items: center;
+		gap: 0.5rem;
+	}
+
+	.embla-dot {
+		width: 1rem;
+		height: 1rem;
+		border: solid 1px var(--color-inactive);
+		border-radius: 50%;
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.embla-dot-selected {
+		border: solid 2px var(--color-text);
 	}
 </style>
