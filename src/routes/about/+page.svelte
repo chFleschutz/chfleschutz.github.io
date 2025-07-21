19cee10733cdea060e98c20c43cb3e7fa88ddc6e
Christoph Fleschutz <ch.fleschutz@gmail.com>
2025-07-21 16:18:21 +0200

    Add additional pages

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import About from '$lib/sections/about.svelte';
+</script>
+
+<section><About /></section>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Projects from '$lib/sections/projects.svelte';
+</script>
+
+<section><Projects /></section>
