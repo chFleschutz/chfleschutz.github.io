bc4d8c9a2a6851175241d81ef79275799dc75198
Christoph Fleschutz <ch.fleschutz@gmail.com>
2025-07-22 13:05:41 +0200

    Add copied email confirmation

--- src/lib/sections/contact.svelte
+++ src/lib/sections/contact.svelte
@@ -6,54 +6,36 @@
 
 	const email = 'ch.fleschutz@gmail.com';
 	let copied = false;
-	const copyEmail = () => {
-		navigator.clipboard.writeText(email);
-		copied = true;
-		setTimeout(() => {
-			copied = false;
-		}, 2000);
-	};
+
+	async function copyEmail() {
+		try {
+			await navigator.clipboard.writeText(email);
+			copied = true;
+			setTimeout(() => (copied = false), 2000); // Reset after 2 seconds
+		} catch (err) {
+			console.error('Failed to copy:', err);
+		}
+	}
 </script>
 
-<div class="contact-container">
-	<div class="contact">
-		<h2 class="text-gradient">Let's Get In Touch</h2>
-		<p>If you have any questions feel free to contact me</p>
-		<div
-			class="email-container hover-effect"
-			on:click={copyEmail}
-			on:keydown={(e) => e.key === 'Enter' && copyEmail()}
-			aria-label="Copy email address"
-			role="button"
-			tabindex="0"
-		>
-			<p>{email}</p>
-			<div>
-				<FontAwesomeIcon icon={faCopy} />
-			</div>
-		</div>
-		<LinkIcons />
-	</div>
+<div class="contact">
+	<h2 class="text-gradient">Let's Get In Touch</h2>
+	<p>If you have any questions feel free to contact me</p>
+	<span class="copied-message" class:visible={copied}>Copied</span>
+	<button class="email-container hover-effect" on:click={copyEmail} aria-label="Copy email">
+		<p>{email}</p>
+		<FontAwesomeIcon icon={faCopy} />
+	</button>
+	<LinkIcons />
 </div>
 
 <style>
-	.contact-container {
-		position: relative;
-		width: 100vw;
-		height: 100vh;
-		display: grid;
-		grid-template-rows: 1fr auto;
-		justify-items: center;
-		align-items: center;
-	}
-
 	.contact {
-		max-width: 70vw;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		text-align: center;
-		margin-top: 3rem;
+		margin: 15rem 0;
 	}
 
 	.email-container {
@@ -61,8 +43,8 @@
 		align-items: center;
 		gap: 1rem;
 		cursor: pointer;
-		padding: 1rem;
-		margin: 2rem 0;
+		padding: 0.75rem 2rem;
+		margin: 0.5rem 0 2rem 0;
 		background-color: transparent;
 		border: 1px solid var(--color-text);
 		border-radius: 10px;
@@ -70,5 +52,18 @@
 
 	.email-container p {
 		margin: 0;
+	}
+
+	.copied-message {
+		font-weight: var(--font-weight-bold);
+		background-color: var(--color-accent);
+		border-radius: 10px;
+		padding: 0.25rem 1rem;
+		opacity: 0;
+		transition: opacity 0.3s ease;
+	}
+
+	.copied-message.visible {
+		opacity: 1;
 	}
 </style>
